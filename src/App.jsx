2fbaba5439fa85740ddd8de8b5b4d420f8565311
aryanas159 <ssaryans597@gmail.com>
2023-02-27 21:13:58 +0530

    made a new compont Question

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,101 @@
-import React from 'react'
-import './App.css'
-
+import React from "react";
+import "./App.css";
+import Question from "./assets/Question";
 
 function App() {
-  const [data, setData] = React.useState([])
-  const [chosenOptions, setChosenOptions] = React.useState({})
-  const [styleState, setStyleState] = React.useState({})
-  const [submitStatus, setSubmitStatus] = React.useState(false)
-  const chosenValues = Object.values(chosenOptions)
-  const correctValues = []
+  const [data, setData] = React.useState([]);
+  const [chosenOptions, setChosenOptions] = React.useState({});
+  const [submitStatus, setSubmitStatus] = React.useState(false);
+  const chosenValues = Object.values(chosenOptions);
+  const correctValues = [];
   for (let i = 0; i < data.length; i++) {
-    correctValues[i] = data[i]["correct_answer"]
+    correctValues[i] = data[i]["correct_answer"];
   }
-  const incorrectValues = []
+  const incorrectValues = [];
   for (let i = 0; i < correctValues.length; i++) {
     if (correctValues[i] !== chosenValues[i]) {
-      incorrectValues.push(chosenValues[i])
+      incorrectValues.push(chosenValues[i]);
     }
   }
   const score = () => {
-    let score = 0
+    let score = 0;
     for (let i = 0; i < chosenValues.length; i++) {
-      if (chosenValues[i] === correctValues[i]) {score++}
+      if (chosenValues[i] === correctValues[i]) {
+        score++;
+      }
     }
-    return score
+    return score;
   }
+
   const handleChange = (e) => {
-    let id = e.target.name
-    let value = e.target.value
+    let id = e.target.name;
+    let value = e.target.value;
     setChosenOptions((prev) => {
       return {
         ...prev,
-        [id]: value
-      }
-    })
-  }
+        [id]: value,
+      };
+    });
+  };
   const optionsArray = (incorrect, correct) => {
-    let newArray = [].concat(incorrect)
-    newArray.push(correct)
+    let newArray = [].concat(incorrect);
+    newArray.push(correct);
     let sortedArray = newArray.sort(() => {
-      return Math.random() - 0.5
-    })
-    return sortedArray
-  }
-
+      return Math.random() - 0.5;
+    });
+    return sortedArray;
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    setSubmitStatus(true)
-  }
-  
+    e.preventDefault();
+    setSubmitStatus(true);
+  };
+
   React.useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple")
-      .then(response => response.json())
-      .then(data => {
-        setData(data["results"])
-        setData(prev => {
-          return prev.map(obj => {
+    fetch(
+      "https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data["results"]);
+        setData((prev) => {
+          return prev.map((obj) => {
             return {
               ...obj,
-              choices: optionsArray(obj["incorrect_answers"], obj["correct_answer"])
-            }
-          })
-        })
-      })
-  }
-  , [])
+              choices: optionsArray(
+                obj["incorrect_answers"],
+                obj["correct_answer"]
+              ),
+            };
+          });
+        });
+      });
+  }, []);
 
-  const styles = {
-    backgroundColor: "lightblue"
-  }
-  const styles_correct = {
-    backgroundColor: "lightgreen"
-  }
-  const styles_incorrect = {
-    backgroundColor: "red"
-  }
   const questions = data.map((question, question_index) => {
-    const options = question.choices
+    const options = question.choices;
     return (
-      <div>
-        <h1>{question["question"]}</h1>
-            <div className="options">
-                    {
-                      options.map((option, index) => {
-                        return (
-                          <div>
-                          <label style={submitStatus ? correctValues.includes(option) ? styles_correct : incorrectValues.includes(option) ? styles_incorrect : {} : chosenValues.includes(option) ? styles : {}}>
-                          <input 
-                              type="radio"
-                              id={index}
-                              name={question_index}
-                              value={option}
-                              onChange={handleChange}
-                              checked={chosenValues.includes(option)}
-                              
-                          />
-                            {option}
-                            </label>
-                          </div>
-                        )
-                      })
-                    }
-                </div>
-      </div>
-    )
-  })
+      <Question
+        question={question["question"]}
+        options_array={options}
+        correct_options_array={correctValues}
+        question_index={question_index}
+        submitStatus={submitStatus}
+        chosenValues={chosenValues}
+        handleChange={handleChange}
+      />
+    );
+  });
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         {questions}
-        <button type='submit'> Submit </button>
+        <button type="submit"> Submit </button>
       </form>
       {submitStatus ? <h3>Score: {score()}</h3> : <></>}
     </div>
-  )
+  );
 }
 
-export default App
-
-
-
+export default App;
--- src/assets/Question.jsx
+++ src/assets/Question.jsx
@@ -1,51 +1,57 @@
-import React from "react"
-export default function Question(props) {
-    const [option, setOption] = React.useState("")
-    const handleChange = (event) => {
-        setOption(event.target.value)
-    }
-    const onSubmit = () => {
-        alert("You have chosen ", option)
-    }
+import React from "react";
+export default function Question({
+    question,
+    options_array,
+    correct_options_array,
+    question_index,
+    submitStatus,
+    chosenValues,
+    handleChange,
+}) {
+    const styles = {
+        backgroundColor: "lightblue",
+    };
+    const styles_correct = {
+        backgroundColor: "lightgreen",
+    };
+    const styles_incorrect = {
+        backgroundColor: "red",
+    };
+
     return (
-        <div className="question">
-            <form onSubmit={onSubmit}>
-                <h1>{props.question}</h1>
-                <div className="options">
-                    <input 
-                        type="radio"
-                        id="option1"
-                        name={props.options[0]}
-                        value={props.options[0]}
-                    
+        <div>
+        <h1>{question}</h1>
+        <div className="options">
+            {options_array.map((option, index) => {
+            return (
+                <div>
+                <label
+                    style={
+                    submitStatus
+                        ? option === correct_options_array[question_index]
+                        ? styles_correct
+                        : chosenValues[question_index] === option
+                        ? styles_incorrect
+                        : {}
+                        : chosenValues[question_index] === option
+                        ? styles
+                        : {}
+                    }
+                >
+                    <input
+                    type="radio"
+                    id={index}
+                    name={question_index}
+                    value={option}
+                    onChange={handleChange}
+                    checked={chosenValues[question_index] === option}
                     />
-                    <label htmlFor="option1">{props.options[0]}</label>
-                    <input 
-                        type="radio"
-                        id="option2"
-                        name={props.options[1]}
-                        value={props.options[1]}
-                    
-                    />
-                    <label htmlFor="option2">{props.options[1]}</label>
-                    <input 
-                        type="radio"
-                        id="option3"
-                        name={props.options[2]}
-                        value={props.options[2]}
-                    
-                    />
-                    <label htmlFor="option3">{props.options[2]}</label>
-                    <input 
-                        type="radio"
-                        id="option4"
-                        name={props.options[3]}
-                        value={props.options[3]}
-                    
-                    />
-                    <label htmlFor="option4">{props.options[3]}</label>
+                    {option}
+                </label>
                 </div>
-            </form>
-            </div>
-    )
+            );
+            })}
+        </div>
+        </div>
+    );
 }
